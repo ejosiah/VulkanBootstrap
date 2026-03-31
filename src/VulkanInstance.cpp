099853a3d2b34c3456ec3472b0db47ace5677f64
Josiah Ebhomenye <joebhomenye@gmail.com>
2026-03-31 08:49:27 +0100

    Validate Vulkan instance setup before creation

--- src/VulkanInstance.cpp
+++ src/VulkanInstance.cpp
@@ -3,9 +3,63 @@
 #include "VulkanDebugMessenger.hpp"
 #include "WindowInterface.hpp"
 #include <spdlog/spdlog.h>
+#include <algorithm>
+#include <cstring>
 #include <iostream>
 #include <stdexcept>
 #include <utility>
+
+namespace {
+std::vector<VkExtensionProperties> enumerateInstanceExtensions() {
+    uint32 count = 0;
+    auto result = vkEnumerateInstanceExtensionProperties(nullptr, &count, nullptr);
+    if(result != VK_SUCCESS) {
+        spdlog::error("failed to enumerate Vulkan instance extensions, vk result={}", static_cast<int>(result));
+        throw std::runtime_error("failed to enumerate Vulkan instance extensions");
+    }
+
+    std::vector<VkExtensionProperties> extensions(count);
+    result = vkEnumerateInstanceExtensionProperties(nullptr, &count, extensions.data());
+    if(result != VK_SUCCESS) {
+        spdlog::error("failed to read Vulkan instance extensions, vk result={}", static_cast<int>(result));
+        throw std::runtime_error("failed to read Vulkan instance extensions");
+    }
+
+    extensions.resize(count);
+    return extensions;
+}
+
+std::vector<VkLayerProperties> enumerateInstanceLayers() {
+    uint32 count = 0;
+    auto result = vkEnumerateInstanceLayerProperties(&count, nullptr);
+    if(result != VK_SUCCESS) {
+        spdlog::error("failed to enumerate Vulkan instance layers, vk result={}", static_cast<int>(result));
+        throw std::runtime_error("failed to enumerate Vulkan instance layers");
+    }
+
+    std::vector<VkLayerProperties> layers(count);
+    result = vkEnumerateInstanceLayerProperties(&count, layers.data());
+    if(result != VK_SUCCESS) {
+        spdlog::error("failed to read Vulkan instance layers, vk result={}", static_cast<int>(result));
+        throw std::runtime_error("failed to read Vulkan instance layers");
+    }
+
+    layers.resize(count);
+    return layers;
+}
+
+bool hasExtension(const std::vector<VkExtensionProperties>& extensions, const char* extensionName) {
+    return std::any_of(extensions.begin(), extensions.end(), [extensionName](const auto& extension) {
+        return std::strcmp(extension.extensionName, extensionName) == 0;
+    });
+}
+
+bool hasLayer(const std::vector<VkLayerProperties>& layers, const char* layerName) {
+    return std::any_of(layers.begin(), layers.end(), [layerName](const auto& layer) {
+        return std::strcmp(layer.layerName, layerName) == 0;
+    });
+}
+}
 
 VulkanInstance::VulkanInstance(const VkInstanceCreateInfo &createInfo,
                                VkApplicationInfo appInfo,
@@ -88,31 +142,68 @@ VulkanInstanceBuilder &VulkanInstanceBuilder::addExtensions(std::vector<const ch
 }
 
 std::shared_ptr<VulkanInstance> VulkanInstanceBuilder::make_shared() {
+    auto enabledExtensions = enabledExtensions_;
+    auto enabledLayers = enabledLayers_;
+    const auto availableExtensions = enumerateInstanceExtensions();
+    const auto availableLayers = enumerateInstanceLayers();
+
+    for(const auto* extension : enabledExtensions) {
+        spdlog::info("Requesting Vulkan instance extension: {}", extension);
+    }
+
+    for(const auto* layer : enabledLayers) {
+        spdlog::info("Requesting Vulkan instance layer: {}", layer);
+    }
+
+#ifndef NDEBUG
+    const bool debugUtilsSupported = hasExtension(availableExtensions, VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    if(debugUtilsSupported &&
+       std::find(enabledExtensions.begin(), enabledExtensions.end(), VK_EXT_DEBUG_UTILS_EXTENSION_NAME) == enabledExtensions.end()) {
+        enabledExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+#endif
+
+    for(const auto* extension : enabledExtensions) {
+        if(!hasExtension(availableExtensions, extension)) {
+            spdlog::error("required Vulkan instance extension is unavailable: {}", extension);
+            throw std::runtime_error("required Vulkan instance extension is unavailable");
+        }
+    }
+
+    for(const auto* layer : enabledLayers) {
+        if(!hasLayer(availableLayers, layer)) {
+            spdlog::error("required Vulkan instance layer is unavailable: {}", layer);
+            throw std::runtime_error("required Vulkan instance layer is unavailable");
+        }
+    }
+
     VkApplicationInfo appInfo{ VK_STRUCTURE_TYPE_APPLICATION_INFO };
     appInfo.pApplicationName = appName_.c_str();
     appInfo.applicationVersion = appVersion_;
     appInfo.pEngineName = engineName_.c_str();
     appInfo.engineVersion = engineVersion_;
-    appInfo.apiVersion = apiVersion_;
-
-#ifndef NDEBUG
-    if(std::find(enabledExtensions_.begin(), enabledExtensions_.end(), VK_EXT_DEBUG_UTILS_EXTENSION_NAME) == enabledExtensions_.end()) {
-        enabledExtensions_.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-    }
-#endif
+    const auto loaderApiVersion = volkGetInstanceVersion();
+    appInfo.apiVersion = loaderApiVersion != 0 ? std::min(apiVersion_, loaderApiVersion) : apiVersion_;
+    spdlog::info("Using Vulkan API version {}.{}.{}",
+                 VK_API_VERSION_MAJOR(appInfo.apiVersion),
+                 VK_API_VERSION_MINOR(appInfo.apiVersion),
+                 VK_API_VERSION_PATCH(appInfo.apiVersion));
 
     VkInstanceCreateInfo createInfo{ VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
     createInfo.pApplicationInfo = &appInfo;
 
-    createInfo.enabledExtensionCount = to<uint32>(enabledExtensions_.size());
-    createInfo.ppEnabledExtensionNames = enabledExtensions_.data();
+    createInfo.enabledExtensionCount = to<uint32>(enabledExtensions.size());
+    createInfo.ppEnabledExtensionNames = enabledExtensions.empty() ? nullptr : enabledExtensions.data();
 
-    createInfo.enabledLayerCount = to<uint32>(enabledLayers_.size());
-    createInfo.ppEnabledLayerNames = enabledLayers_.data();
+    createInfo.enabledLayerCount = to<uint32>(enabledLayers.size());
+    createInfo.ppEnabledLayerNames = enabledLayers.empty() ? nullptr : enabledLayers.data();
 
 #ifndef NDEBUG
-    auto debugInfo = VulkanDebugMessenger::debugCreateInfo();
-    createInfo.pNext = &debugInfo;
+    VkDebugUtilsMessengerCreateInfoEXT debugInfo{};
+    if(debugUtilsSupported) {
+        debugInfo = VulkanDebugMessenger::debugCreateInfo();
+        createInfo.pNext = &debugInfo;
+    }
 #endif
     VkInstance instance{};
 
@@ -122,7 +213,7 @@ std::shared_ptr<VulkanInstance> VulkanInstanceBuilder::make_shared() {
         throw std::runtime_error("unable to create Vulkan instance");
     }
 
-    auto appInstance = std::make_shared<VulkanInstance>(createInfo, appInfo, enabledLayers_, enabledExtensions_, instance);
+    auto appInstance = std::make_shared<VulkanInstance>(createInfo, appInfo, std::move(enabledLayers), std::move(enabledExtensions), instance);
     if(window_){
         appInstance->set(window_->createSurface(instance));
     }
